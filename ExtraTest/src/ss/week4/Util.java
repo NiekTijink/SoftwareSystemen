54c96dda3771323189f685c6c3f8356c7f240b33
Thomas <t.kolner@student.utwente.nl>
2015-12-02 11:03:24 +0100

    Change util, ELEMENT not correct.

--- ExtraTest/src/ss/week4/Util.java
+++ ExtraTest/src/ss/week4/Util.java
@@ -4,26 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Util {
-    public static <Element> List<Element> zip
-    (List<Element> l1, List<Element> l2) {
-        ArrayList<Element> RESULT = new ArrayList<Element>();
-        for (int i = 0; i<l1.size(); i++) {
-            RESULT.add(l1.get(i));
-            RESULT.add(l2.get(i));
+    public static <ELEMENT> List<ELEMENT> zip(
+    				List<ELEMENT> l1, List<ELEMENT> l2) {
+        ArrayList<ELEMENT> rESULT = new ArrayList<ELEMENT>();
+        for (int i = 0; i < l1.size(); i++) {
+            rESULT.add(l1.get(i));
+            rESULT.add(l2.get(i));
         }
-        return RESULT;
+        return rESULT;
     }    
 
-    /**
-     * signum function
+    /** signum function.
      * @param i the function argument
      * @return -1, 0 or 1 if the argument is negative, 0 or positive
      */
     public static int signum(int i) {
-        if(i < 0)
-            return -1;
-        else if (i > 0)
-            return 1;
-        else return 0;
+        if (i < 0) 	{ 
+        	return -1; 
+        } else if (i > 0) {
+        	return 1;
+        } else {
+        	return 0;
+        }
     }
 }
