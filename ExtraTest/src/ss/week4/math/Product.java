0a4db9da1228e4fbb3b81feaaf72d45711924134
Thomas <t.kolner@student.utwente.nl>
2015-12-02 11:43:43 +0100

    Product done.

---
+++ ExtraTest/src/ss/week4/math/Product.java
@@ -0,0 +1,20 @@
+package ss.week4.math;
+
+public class Product implements Function {
+	private Function a;
+	private Function b;
+	
+	public Product(Function a, Function b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public double apply(double num) {
+		return a.apply(0) * b.apply(0);
+	}
+	
+	public Function derivative() {
+		return new Sum( new Product(a.derivative(), b), new Product(b.derivative(), a));
+		
+	}
+}
