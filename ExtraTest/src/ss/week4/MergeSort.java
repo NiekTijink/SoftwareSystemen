c370aa7d9023dac3913c3e79223a424d32a6d912
Thomas <t.kolner@student.utwente.nl>
2015-12-03 11:43:48 +0100

    Bug

--- ExtraTest/src/ss/week4/MergeSort.java
+++ ExtraTest/src/ss/week4/MergeSort.java
@@ -5,6 +5,13 @@ import java.util.*;
 public class MergeSort {
     public static <Elem extends Comparable<Elem>>
            void mergesort(List<Elem> list) {
+    	int halfsize = list.size() / 2;
+    	int otherhalf = list.size() - halfsize;
+    	
+    	int[] arr1;
+    	for (int i = 0; i < halfsize; i++) {
+    		arr1[i] = ((int) list.get(i));
+    	}
     	// TODO: implement, see exercise P-4.16
     }
 }
