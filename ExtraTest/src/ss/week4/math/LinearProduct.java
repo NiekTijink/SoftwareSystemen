9a6c10f117ad135ca5bc14b566a56a1c9cf0082b
Niek Tijink <Niek Tijink@130.89.95.57>
2015-12-03 11:15:24 +0100

    Begin Sorting

--- ExtraTest/src/ss/week4/math/LinearProduct.java
+++ ExtraTest/src/ss/week4/math/LinearProduct.java
@@ -1,22 +1,19 @@
 package ss.week4.math;
 
 public class LinearProduct extends Product implements Integrandable {
-	private Function a;
-	private Constant b;
-	
+		
 	public LinearProduct(Function a, Constant b) {
 		super(a, b); 
-		this.a = a;
-		this.b = b;
+		
 	}
 	
 	public Function derivative() {
-		return new LinearProduct(a.derivative(), b);
+		return new LinearProduct(a.derivative(), (Constant) b);
 	}
 	
 	public Function integrand() {
 		if (a instanceof Integrandable) {
-			return new LinearProduct(((Integrandable) a).integrand(), b);
+			return new LinearProduct(((Integrandable) a).integrand(), (Constant) b);
 		} else {
 			return null;
 		}
