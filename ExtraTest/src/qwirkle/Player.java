eea91f33176f65c733c7590d0120faea0aa1749d
Niek Tijink <Niek Tijink@Niek>
2016-01-06 11:31:40 +0100

    a

---
+++ ExtraTest/src/qwirkle/Player.java
@@ -0,0 +1,15 @@
+package qwirkle;
+
+public class Player {
+    private String name;
+    private Tile[] hand;
+
+    
+    public String getName() {
+    	return name;
+    }
+    
+    public Tile[] getHand() {
+    	return hand;
+    }
+}
--- ExtraTest/src/qwirkle/Tile.java
+++ ExtraTest/src/qwirkle/Tile.java
@@ -1,5 +1,5 @@
 package qwirkle;
 
 public class Tile {
-
+	
 }
